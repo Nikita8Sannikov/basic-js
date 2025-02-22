0dc562c4272d048b5b4bf3b540ee89107c5dbd5a
Nikita8Sannikov <Ak-4t7@mail.ru>
2025-02-22 17:10:18 +0600

    feat: add solotion DreamTeam, HanoiTower and WhatSeason

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,10 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if(!Array.isArray(members) ) return false
+  const filteredMembers = members.filter(el => typeof el === 'string')
+  return filteredMembers.map(el => el.trim().slice(0,1)).join('').toUpperCase().split('').sort().join('')
 }
 
 module.exports = {
